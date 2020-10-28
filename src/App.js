2916ec8833b426709175f3fd1dda164e1d48ce63
ERIC-YU-LIAO <ERIC-YU-LIAO@users.noreply.github.com>
2020-10-29 00:39:28 +0800

    1029

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './component/homepage/HomePage'
+
+
+const App = () =>{
+  return (
+    <div>
+    <HomePage/>
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/component/homepage/HomePage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import '../homepage/homepage.scss'
+
+const HomePage = () =>{
+   return   (  <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className="title"> Hat</h1>
+                        <span className="subtitle"> SHPA</span>
+                    </div>
+                </div>
+                {/*  */}
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className="title"> Hat</h1>
+                        <span className="subtitle"> SHPA</span>
+                    </div>
+                </div>
+                {/*  */}
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className="title"> Hat</h1>
+                        <span className="subtitle"> SHPA</span>
+                    </div>
+                </div>
+                {/*  */}
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className="title"> Hat</h1>
+                        <span className="subtitle"> SHPA</span>
+                    </div>
+                </div>
+                {/*  */}
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className="title"> Hat</h1>
+                        <span className="subtitle"> SHPA</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+          )
+}
+
+
+export default HomePage
