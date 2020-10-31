d175155696257a18418ad6191788a0332ae1f1c6
Eric <>
2020-10-31 16:24:52 +0800

    1031

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './component/homepage/HomePage'
+import {Route} from "react-router-dom";
 
+
+const Hat =()=>{
+  return <p>Hat</p>
+}
 
 const App = () =>{
   return (
     <div>
-    <HomePage/>
+    <Route exact path="/" component={HomePage}/>
+    <Route path="/mens" component={Hat}/>
     </div>
   )
 }
--- src/component/menuitem/menuItem.js
+++ src/component/menuitem/menuItem.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import '../menuitem/menuitem.scss'
+import React from 'react';
+import '../menuitem/menuitem.scss';
+import { withRouter } from "react-router";
 
 
-const MenuItem = ({id,title,imageUrl} ) =>{
-    return(
-    <div style={{
-         backgroundImage: `url( ${imageUrl})` }}
-         className='menu-item'>
-        <div className='content'>
-            <h1 className="title"> {title}</h1>
-            <span className="subtitle"> {id}</span>
-        </div>
+
+// history 物件 指向match 屬性下方 match.url 而url去父層定義去傳下每個 MenuItem 
+
+const MenuItem = ({ title, imageUrl, size, history,match,linkUrl}) => (
+  <div className={`${size} menu-item`} onClick={()=>{history.push(`${match.url}${linkUrl}`)}}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-   
-    )
-}
+  </div>
+);
 
-export default MenuItem
+export default withRouter(MenuItem);
